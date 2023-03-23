c5cbd13ec9777305015117a493effe027d5fa31a
Matthias <matthias_droste@gmx.de>
2023-03-23 18:31:01 +0100

    Client Rebuilt Client, Changed update fetch cycle

--- clientgui/src/main/java/org/cardsagainsthumanity/client/CardArea.java
+++ clientgui/src/main/java/org/cardsagainsthumanity/client/CardArea.java
@@ -7,10 +7,9 @@ import java.awt.event.MouseEvent;
 
 public class CardArea extends JPanel {
 
-    private int padding_w = 5;
-    private WhiteCard[] wcarr;
+    private WhiteCard[] wcArr;
 
-    private Runner runner;
+    private final Runner runner;
 
     public CardArea(Runner r){
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
@@ -19,7 +18,7 @@ public class CardArea extends JPanel {
     }
     public void setWc(WhiteCard[] wc, String type){
         this.removeAll();
-        wcarr = wc;
+        wcArr = wc;
         addCards(wc, type);
     }
 
@@ -30,19 +29,20 @@ public class CardArea extends JPanel {
 
     public int getDrawWidthSingleComponent(String type){
         int width = 0;
+        int padding_w = 5;
         switch(type){
             case "BlackCard":
-                width = (int)(this.getSize().getWidth()-padding_w);
+                width = (int)(this.getSize().getWidth()- padding_w);
                 return width;
             case "WhiteCard":
                 int count = 0;
-                for(WhiteCard wc : wcarr){
+                for(WhiteCard wc : wcArr){
                     if(wc != null){
                         count++;
                     }
                 }
-                int entirepadding_w = padding_w*count;
-                width = (int)(this.getSize().getWidth()-entirepadding_w)/count;
+                int entirePadding_w = padding_w *count;
+                width = (int)(this.getSize().getWidth()-entirePadding_w)/count;
                 return width;
             default:
                 return 0;
@@ -61,8 +61,10 @@ public class CardArea extends JPanel {
                 public void mousePressed(MouseEvent e) {
                     if (type.equals("putCards")) {
                         runner.putCard(wc.getIndex());
+                        wc.setVisible(false);
                     }else{
                         runner.playCard(wc.getIndex());
+                        wc.setVisible(false);
                     }
                 }
                 @Override
